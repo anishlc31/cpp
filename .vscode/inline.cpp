e08450d3dbeb72dafbccacc9905aded39cdef838
Anish Lamichhane <codingonly31@gmail.com>
2023-02-28 13:16:55 +0545

    inline

---
+++ .vscode/inline.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+ 
+  inline void sum( int a=3, int b=7){
+    int s ;
+    s=a+b;
+    cout << " sum = "<< s;
+  }
+
+  int main (){
+    sum();
+  }
