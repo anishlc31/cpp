7d4083c3a55cf263ae5f56f5f250f5858052c0aa
Anish Lamichhane <codingonly31@gmail.com>
2023-02-28 13:28:44 +0545

    over

---
+++ fuction/overloading.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+ 
+  int mul(int a ,int b){
+    return(a*b);
+
+  }
+
+    int mul(float a ,float b){
+    return(a*b);
+
+  }
+
+int main(){
+    int x=5,y=6;
+    cout << mul(x,y);
+
+    
+}
+  
