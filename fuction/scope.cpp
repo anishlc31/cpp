ca00c52442f684811e7b2bdb762338f220a46d0a
Anish Lamichhane <codingonly31@gmail.com>
2023-02-28 15:40:08 +0545

    scope

---
+++ fuction/scope.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int g=20;
+int main(){
+    int i=10;
+    if(i<20){
+        int n=100;
+        cout<<g<<endl;
+        cout<<i;
+        cout<<n;
+    }
+     cout<<g;
+        cout<<i;
+
+}
